4f38d29b712d8e84f08c2d0972d6dbd65df3fff5
most-trusted <hapandiohome@gmail.com>
2026-02-22 14:39:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI, UploadFile, File, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-import pandas as pd
 from backtester import run_backtest
-import io
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -14,9 +12,5 @@ async def home(request: Request):
 
 @app.post("/backtest")
 async def backtest(file: UploadFile = File(...)):
-    contents = await file.read()
-    df = pd.read_csv(io.StringIO(contents.decode("utf-8")))
-
-    results = run_backtest(df)
-
+    results = run_backtest(file.file)  # pass file-like object
     return results
